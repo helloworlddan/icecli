7c1ae55940a54140a7fb20a5c566c2e228377156
Daniel Stamer <dan@hello-world.sh>
2022-10-11 17:24:51 +0200

    initial commit
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/helloworlddan/icecli
+
+go 1.18
---
+++ icecli.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const (
+	version = "0.0.1"
+)
+
+func main() {
+	fmt.Printf("icecli version %s\n", version)
+}
